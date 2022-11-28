13e78411ed2aada3ed3e6f96ba591917ab4cfd01
Eugene Morozov <jmv@emorozov.net>
2022-11-28 16:28:46 +0300

    Updates config for new patches.

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
@@ -42,6 +43,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -70,13 +72,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *cmdbrightnessup[]  = { "brightnessctl", "s", "10%+", NULL };
 static const char *cmdbrightnessdown[]  = { "brightnessctl", "s", "10%-", NULL };
 static const char *cmdsoundmute[]  = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *cmdsoundup[]  = { "change-volume",  "2db+", "unmute", NULL };
 static const char *cmdsounddown[]  = { "change-volume",  "2db-", NULL };
 static const char *cmdsuspend[] = { "systemctl", "suspend", NULL };
-static const char *cmdlock[] = { "betterlockscreen", "-l", "dim", NULL };
+static const char *cmdlock[] = { "slock", NULL };
 static const char *cmdscreenshot[] = { "flameshot", "gui", NULL };
 static const char *cmdrofimoji[] = { "rofimoji", NULL };
 
@@ -84,6 +88,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = cmdlock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ 0,                            XF86MonBrightnessDown,     spawn,         {.v = cmdbrightnessdown } },
