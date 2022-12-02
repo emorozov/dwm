916a80bea9a8625ae46ff4478f94f8ca4f9b2ec0
Eugene Morozov <jmv@emorozov.net>
2022-12-02 17:19:42 +0300

    Attempt to fix named scratchpad configuration.

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /*static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" };*/
-static char *fonts[]          = { "monospace:size=10", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
+static char *fonts[]          = { "CaskaydiaCove Nerd Font Mono:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -32,11 +32,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",            NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "jetbrains-pycharm",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Anki",               NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "telegram-desktop",   NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx  scratch key */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1,        50,50,500,500,        5,   0 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,        50,50,500,500,        5,   0 },
+	{ NULL,       NULL,       "scratchpad",  1 << 8,    0,           -1,        50,50,500,500,        5,   's' },
 };
 
 /* layout(s) */
@@ -73,7 +72,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "s", "alacritty", "-t", scratchpadname, NULL };
 static const char *cmdbrightnessup[]  = { "brightnessctl", "s", "10%+", NULL };
 static const char *cmdbrightnessdown[]  = { "brightnessctl", "s", "10%-", NULL };
 static const char *cmdsoundmute[]  = { "pactl", "set-sink-mute", "0", "toggle", NULL };
